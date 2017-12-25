20515639c115ba33492c6c5b53475b3da5c553dd
Onur Eker <yonureker@gmail.com>
2017-12-25 02:26:09 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,7 +8,7 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   the_manifest = {}
-  the_manifest["whale bone sorsets"]= 5
+  the_manifest["whale bone corsets"]= 5
   the_manifest["porcelain vases"]= 2
   the_manifest["oil paintings"]= 3
 end
